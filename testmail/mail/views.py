943eb3900cb143b9c8e8b21b9fd7caab2d37156c
devignesh <vinesh1865@gmail.com>
2019-03-12 14:30:14 +0530

    Adding send_mail func

--- testmail/mail/views.py
+++ testmail/mail/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
+
+def index(request):
+
+    send_mail('Test mail for Django by Vicky',
+    'Django test mail',
+    'vigneshkumar.mca2016@adhiyamaan.in',
+    ['vinesh1865@gmail.com','surjithkumar.mca2016@adhiyamaan.in','mca2016b@adhiyamaan.in','manigandan.jeff@gmail.com'],
+    fail_silently=False)
+    return render(request, 'mail/index.html')
